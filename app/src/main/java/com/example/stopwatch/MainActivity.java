fd35808fbdc96361c9784f5f4878d58a77a2512d
JyotikaJayani-08 <158709375+JyotikaJayani-08@users.noreply.github.com>
2024-04-02 19:09:31 +0530

    Initial commit

---
+++ app/src/main/java/com/example/stopwatch/MainActivity.java
@@ -0,0 +1,104 @@
+package com.example.stopwatch;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.PersistableBundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity {
+
+    private int seconds;
+    private boolean running;
+    private boolean wasRunning;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        if(savedInstanceState != null){
+            savedInstanceState.getInt("seconds");
+            savedInstanceState.getBoolean("running");
+            savedInstanceState.getBoolean("wasRunning");
+
+        }
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        runTimer();
+    }
+
+    public void onStart(View view){
+        running = true;
+    }
+    public void onStop(View view){
+        running = false;
+    }
+    public void onReset(View view){
+        running = false;
+        seconds = 0;
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        wasRunning = running;
+        running = false;
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if(wasRunning){
+            running  = true;
+        }
+    }
+
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+
+        outState.putInt("seconds",seconds);
+        outState.putBoolean("running",running);
+        outState.putBoolean("wasRunning",wasRunning);
+
+    }
+
+    private void runTimer() {
+        TextView timeView = findViewById(R.id.textView);
+        Handler handler = new Handler();
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                int hours = seconds / 3600;
+                int minutes = (seconds % 3600) / 60;
+                int secs = seconds % 60;
+
+                String time = String.format(Locale.getDefault(),
+                                    "%d:%02d:%02d",
+                                      hours,minutes,secs);
+                timeView.setText(time);
+
+                if (running) {
+                    seconds++;
+                }
+                handler.postDelayed(this,1000);
+
+            }
+        });
+    }
+}
